8a1086c611d49e41bb778862afba517d0ab0914f
Ihor Palii <palii.ihor@uvik.net>
2025-06-21 09:04:34 +0300

    gRPC rust server

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+pub mod hello {
+    tonic::include_proto!("hello");
+}
+
+use hello::{HelloRequest, HelloReply};
+use hello::greeter_server::{Greeter, GreeterServer};
+use tonic::{transport::Server, Request, Response, Status};
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let reply = HelloReply {
+            message: format!("Hello, {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
